ccdf205142eca15858f949a84a9138fa706af20e
evanskosgei <evanskosgei090@gmail.com>
2024-09-25 12:23:37 +0300

    nav change

--- src/dashboard/components/navbar.jsx
+++ src/dashboard/components/navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
                     <Link to="/home" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <span className="self-center text-2xl font-semibold whitespace-nowrap">
-                            <span className="font-extrabold text-white bg-red-500 p-2 rounded-full" style={{ width: '2rem', height: '2rem', display: 'inline-flex', justifyContent: 'center', alignItems: 'center' }}>V</span>
+                            <span className="font-extrabold text-white bg-green-800 p-2 rounded-full" style={{ width: '2rem', height: '2rem', display: 'inline-flex', justifyContent: 'center', alignItems: 'center' }}>V</span>
                             write
                         </span>
                     </Link>
@@ -26,7 +26,7 @@ const Navbar = () => {
             <nav className="bg-gray-50">
                 <div className="max-w-screen-xl px-4 py-3 mx-auto">
                     <div className="flex items-center font-semibold text-lg">
-                        <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+                        <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-md">
                             <li>
                                 <a href="/home" className="text-gray-900 hover:underline focus:text-blue-500 active:text-blue-500" aria-current="page">Home</a>
                             </li>
